6060f65b95a6fdc6175881a24ee1e60bca4d7a13
mananapr <manny011@live.com>
2019-04-20 05:03:02 +0530

    Added payment gateway

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-
 var express = require("express");
 var app = express();
 var mysql = require('mysql')
 var bcrypt = require('bcrypt')
 var bodyParser = require("body-parser");
+var Insta = require('instamojo-nodejs');
 
 
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'password',
+  password : 'root',
   database : 'snuTreats'
 });
+connection.connect()
 
+// for bcrypt
 var saltRounds = 5;
 
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended:true}));
 
-
-connection.connect()
+Insta.setKeys('API_KEY', 'AUTH_KEY');
+Insta.isSandboxMode(true);
 
 //shops 
 app.get("/shops",function(req,res){
@@ -105,7 +107,47 @@ app.post("/register",function(req,res){
 	});
 
 
+// This route returns the link to payment gateway
+app.post("/getPaymentLink", function(req, res){
 
+    var fname = req.body.firstname;
+    var email  = req.body.email;
+    var amount = req.body.amount;
+    var productinfo = "food";
+
+    var data = new Insta.PaymentData();
+    data.purpose = productinfo;
+    data.amount = amount;
+    data.buyer_name = fname;
+    data.email = email;
+    data.setRedirectUrl('http://localhost:3000/insta');
+
+    Insta.createPayment(data, function(error, response) {
+      if (error) {
+        // some error
+        console.log(error);
+      } else {
+        // Payment redirection link at response.payment_request.longurl
+        res.send(response);
+      }
+    });
+
+});
+
+
+// This route handles a successful transaction
+app.get("/insta", function(req, res){
+
+    var paymentID = req.query.payment_id;
+    var requestID = req.query.payment_request_id;
+    var paymentStatus = req.query.payment_status;
+    connection.query('INSERT INTO `payments` (`paymentID`,`status`, `requestID`) VALUES ("'+paymentID+'", "'+paymentStatus+'", "'+requestID+'")',function (error, results, fields) {
+            if(error)
+                console.log(error)
+            res.send("success");
+    });
+
+});
 
 
 app.listen(3000,function(){
